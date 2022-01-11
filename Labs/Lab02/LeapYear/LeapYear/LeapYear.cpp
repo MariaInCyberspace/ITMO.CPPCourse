e5aaadd394a384f4f49582b14f46d5c936de0192
Maria Spirid <lan194@hotmail.com>
2022-01-12 02:05:39 +0300

    Ex. 1 of Lab 02

---
+++ Labs/Lab02/LeapYear/LeapYear/LeapYear.cpp
@@ -0,0 +1,25 @@
+﻿// LeapYear.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+void isLeapYear(int year) {
+    if (year % 4 == 0 && year % 100 != 0) {
+        std::cout << year << " is a leap year!\n";
+    }
+    else if (year % 400 == 0) {
+        std::cout << year << " is a leap year!\n";
+    }
+    else {
+        std::cout << year << " is not a leap year\n";
+    }
+}
+
+int main()
+{
+    std::cout << "Input a year, please:\n";
+    int year;
+    std::cin >> year;
+    isLeapYear(year);
+}
+
