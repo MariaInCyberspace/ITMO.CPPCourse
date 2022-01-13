6efe762bdb1391f40938a4b99830954ba62d20a3
Maria Spirid <lan194@hotmail.com>
2022-01-14 01:46:23 +0300

    Ex. 2 of Lab 3

---
+++ Labs/Lab03/CubeRoot/CubeRoot/CubeRoot.cpp
@@ -0,0 +1,26 @@
+﻿// CubeRoot.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+double calculateCubeRootEasy(double num) {
+    return pow(num, 1.0 / 3);
+}
+
+double calculateCubeRootIter(double num) {
+    double epsilon = 0.0000001;
+    double x = num;
+    while (x * x * x - num > epsilon) {
+        x = (1.0 / 3) * (num / (x * x) + 2 * x);
+    }
+
+    return x;
+}
+
+
+int main()
+{
+    double root = calculateCubeRootIter(216);
+    std::cout << root << "\n";
+}
+
