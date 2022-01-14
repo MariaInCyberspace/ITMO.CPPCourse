ba6e6e6a24050691f0ec07ad5010803a7573f9a0
Maria Spirid <lan194@hotmail.com>
2022-01-14 07:32:03 +0300

    Ex. 1 of Lab 4

---
+++ Labs/Lab04/RootsOfQuadraticEquation/RootsOfQuadraticEquation/RootsOfQuadraticEquation.cpp
@@ -0,0 +1,41 @@
+﻿// RootsOfQuadraticEquation.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int findRootsOfAQuadraticEquation(double a, double b, double c, double& res1, double& res2)
+{
+    double determinant = pow(b, 2) - 4 * a * c;
+    double x, x1, x2, imag;
+    if (determinant > 0)
+    {
+        x1 = (-b + sqrt(determinant)) / (2 * a);
+        x2 = (-b - sqrt(determinant)) / (2 * a);
+        res1 = std::ceil(x1 * 100.0) / 100.0;
+        res2 = std::ceil(x2 * 100.0) / 100.0;
+        return 1;
+    }
+    else if (determinant < 0)
+    {
+        x = -b / (2 * a);
+        imag = sqrt(-determinant) / (2 * a);
+        res1 = std::ceil(x * 100.0) / 100.0;
+        res2 = std::ceil(imag * 100.0) / 100.0;
+        return -1;
+    }
+    else
+    {
+        x = (-b + sqrt(determinant)) / (2 * a);
+        res1 = std::ceil(x * 100.0) / 100.0;
+        res2 = res1;
+        return 0;
+    }
+}
+
+
+int main()
+{
+    double res1, res2;
+    findRootsOfAQuadraticEquation(4, 5, 1, res1, res2);
+    std::cout << res1 << ", "<< res2 << "\n";
+}
