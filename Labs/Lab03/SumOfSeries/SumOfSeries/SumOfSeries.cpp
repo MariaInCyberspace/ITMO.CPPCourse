e21892872d3d8c14b4bc67ae95437c6ab86ad1dd
Maria Spirid <lan194@hotmail.com>
2022-01-14 04:20:14 +0300

    Ex. 4 of Lab 3

--- Labs/Lab03/SumOfSeries/SumOfSeries/SumOfSeries.cpp
+++ Labs/Lab03/SumOfSeries/SumOfSeries/SumOfSeries.cpp
@@ -3,24 +3,36 @@
 
 #include <iostream>
 
-int sumOfSeries(int num, int mul, int step) {
-    if (num > 0) {
-        return num * mul + sumOfSeries(num - step, mul, step);
+//int sumOfSeries(int num, int mul, int step) {
+//    if (num > 0) {
+//        return num * mul + sumOfSeries(num - step, mul, step);
+//    }
+//    else {
+//        return num;
+//    }
+//}
+
+int sumOfSeries(int start, int end, int mult, int step) {
+    if (end >= start) {
+        return start * mult + sumOfSeries(start + step, end, mult, step);
     }
     else {
-        return num;
+        return 0;
     }
 }
 
+
 int main()
 {
-    int start, step, mult;
-    std::cout << "Please enter a positive number to start with:\n";
+    int start, step, mult, end;
+    std::cout << "Enter a positive number to start with:\n";
     std::cin >> start;
+    std::cout << "Enter a positive number to end with:\n";
+    std::cin >> end;
     std::cout << "Enter a positive number to multiply the members of this series by:\n";
     std::cin >> mult;
-    std::cout << "Enter a positive number to decrease by:\n";
+    std::cout << "Enter a positive number to step by:\n";
     std::cin >> step;
-    int res = sumOfSeries(start, mult, step);
+    int res = sumOfSeries(start, end, mult, step);
     std::cout << res << "\n";
 }
