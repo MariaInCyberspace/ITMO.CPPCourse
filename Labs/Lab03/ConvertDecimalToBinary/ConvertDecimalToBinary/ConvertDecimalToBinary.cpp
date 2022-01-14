e9bb7f73bdcb493bbdc9d0e93ab1978a2ba138e5
Maria Spirid <lan194@hotmail.com>
2022-01-14 18:53:54 +0300

    Ex. 5 of Lab 3

--- Labs/Lab03/ConvertDecimalToBinary/ConvertDecimalToBinary/ConvertDecimalToBinary.cpp
+++ Labs/Lab03/ConvertDecimalToBinary/ConvertDecimalToBinary/ConvertDecimalToBinary.cpp
@@ -4,39 +4,34 @@
 #include <iostream>
 #include <vector>
 
-void getBinaryRepresentation(int num, std::vector<int> &binary) {
+void convertToBinary(int num, long& binary, int count = 0) {
     int remainder;
     if (num > 0) {
         remainder = num % 2;
-        getBinaryRepresentation(num / 2, binary);
-        binary.push_back(remainder);
+        count++;
+        convertToBinary(num / 2, binary, count);
+        binary += remainder * pow(10, count - 1);
     }
     else if (num < 0) {
         remainder = num % 2;
-        getBinaryRepresentation(num / 2, binary);
-        binary.push_back(remainder);   
+        count++;
+        convertToBinary(num / 2, binary, count);
+        binary += remainder * pow(10, count - 1);
     }
 }
 
 long convertToBinary(int num) {
-    long b = 0;
-    std::vector<int> binary;
-    getBinaryRepresentation(num, binary);
-    for (int i = 0; i < binary.size(); i++) {
-        b += binary[i] * pow(10, binary.size() - 1 - i);
-    };
-    return b;
+    long binary = 0;
+    convertToBinary(num, binary);
+    return binary;
 }
 
 
 int main()
 {
     int input;
-    long inBinary;
     std::cout << "Input a number, please:\n";
     std::cin >> input;
-    inBinary = convertToBinary(input);
+    long inBinary = convertToBinary(input);
     std::cout << input << " in binary is '" << inBinary << "'\n";
-
 }
-
