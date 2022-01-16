eb21aab69c2f2337190608fff503d709bc0b08c4
Maria Spirid <lan194@hotmail.com>
2022-01-16 07:36:25 +0300

    Mandatory ex. 2 of Lab 5

---
+++ Labs/Lab05/5_2ReturnAnArray/5_2ReturnAnArray/5_2ReturnAnArray.cpp
@@ -0,0 +1,30 @@
+﻿// 5_2ReturnAnArray.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+int* getMaxNumbers(int, int*, int*);
+
+int* getMaxNumbers(int size, int* a, int* b) {
+    int* result = new int[size];
+    for (int i = 0; i < size; i++) {
+        result[i] = a[i] > b[i] ? a[i] : b[i];
+    }
+    return result;
+}
+
+void testFunc() {
+    int a[] = { 1,2,3,4,5,6,7,2 };
+    int b[] = { 7,6,5,4,3,2,1,3 };
+    int s = sizeof(a) / sizeof(int);
+    int* c = getMaxNumbers(s, a, b);
+    for (int i = 0; i < s; i++) {
+        std::cout << c[i] << " ";
+    }
+    std::cout << std::endl;
+    delete[] c;
+}
+
+int main()
+{
+    testFunc();
+}
