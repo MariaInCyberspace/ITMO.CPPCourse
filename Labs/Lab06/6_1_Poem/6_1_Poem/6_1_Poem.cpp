066968f7b0991dc446f48ef281a80c3b32e2f0b0
Maria Spirid <lan194@hotmail.com>
2022-01-18 07:26:18 +0300

    Ex. 1 of Lab 6

---
+++ Labs/Lab06/6_1_Poem/6_1_Poem/6_1_Poem.cpp
@@ -0,0 +1,30 @@
+﻿// 6_1_Poem.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ofstream fout("poem.txt");
+    if (!fout) {
+        cout << "Failed to open file";
+    }
+    char str[130];
+    char read[130];
+    cout << "Hope you can rhyme:\n";
+    cin.get(str, sizeof(str), '&');
+    fout.write(str, sizeof(str) / sizeof(char));
+    fout.close();
+
+    ifstream in("poem.txt");
+    if (!in) {
+        cout << "Failed to open file";
+    }
+    in.read(read, sizeof(read));
+    for (int i = 0; i < sizeof(read); i++) {
+        cout << read[i] << "";
+    }
+    in.close();
+}
