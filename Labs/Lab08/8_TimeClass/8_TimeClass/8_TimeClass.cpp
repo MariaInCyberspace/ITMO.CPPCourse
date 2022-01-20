af6cd76f7ae4730e1c95bf6d1a3b0e80c59bc65c
Maria Spirid <lan194@hotmail.com>
2022-01-20 11:35:31 +0300

    Mandatory ex. 1 of Lab 8

--- Labs/Lab08/8_TimeClass/8_TimeClass/8_TimeClass.cpp
+++ Labs/Lab08/8_TimeClass/8_TimeClass/8_TimeClass.cpp
@@ -11,7 +11,12 @@ Time convert(std::string input) {
 
 int main()
 {
-    Time t(23, 45, 54);
+    char input[9];
+    std::cout << "\nInput time: (hh:mm:ss)\n";
+    std::cin.getline(input, 9);
+    std::string s = &input[0];
+
+    Time t = convert(s);
     t.display();
     t.displayHalf();
     Time t1(11, 15, 45);
@@ -21,24 +26,4 @@ int main()
     //Time t4 = t3.subtractTimeIntervals(t3, t1);
     t3.display();
     t4.display();
-
-    char input[9];
-    std::cout << "\nInput time: (hh:mm:ss)\n";
-    std::cin.getline(input, 9);
-    std::string s = &input[0];
-
-    Time tt = convert(s);
-
-    std::cout << tt.getHours() << ":" << tt.getMinutes() << ":" << tt.getSeconds() << std::endl;
-    Time to_add(21, 44, 50);
-    Time ttt = Time::addTimeIntervals(tt, to_add);
-    std::cout << ttt.getHours() << ":" << ttt.getMinutes() << ":" << ttt.getSeconds() << std::endl;
-
-    Time ti{ 21, 10, 38 };
-    Time ti2{ 24, 10, 38 };
-    Time ti3 = Time::addTimeIntervals(ti, ti2);
-    std::cout << ti3.getHours() << ":" << ti3.getMinutes() << ":" << ti3.getSeconds() << std::endl;
-    Time tti(15, 32, 64);
-    std::cout << tti.getHours() << ":" << tti.getMinutes() << ":" << tti.getSeconds() << std::endl;
-    
 }
