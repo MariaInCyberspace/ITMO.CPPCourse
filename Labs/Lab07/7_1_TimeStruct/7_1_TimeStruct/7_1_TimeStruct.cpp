977d4c28727287720bdddc47f897343ce6fd32c7
Maria Spirid <lan194@hotmail.com>
2022-01-20 23:51:42 +0300

    .

--- Labs/Lab07/7_1_TimeStruct/7_1_TimeStruct/7_1_TimeStruct.cpp
+++ Labs/Lab07/7_1_TimeStruct/7_1_TimeStruct/7_1_TimeStruct.cpp
@@ -3,66 +3,72 @@
 
 #include <iostream>
 #include <string>
+#include <tuple>
 
 struct Time {
     int hours = 0;
     int minutes = 0;
     int seconds = 0;  
 public:
+    Time() { }
     Time(int h, int m, int s) {
-        hours = h;
-        minutes = m;
-        seconds = s;
-        if (s >= 60) {
-            seconds -= 60;
-            minutes += 1;
-        }
-        if (m >= 60) {
-            minutes -= 60;
-            hours += 1;
-        }
-        if (h >= 24) {
-            hours -= 24;
-        }
-
+        int hh, mm, ss;
+        std::tie(hh, mm, ss) = convertToTime_tuple(h, m, s);
+        hours = hh;
+        minutes = mm;
+        seconds = ss;   
     }
-    Time& addTimeInterval(const Time& timeToAdd) {
-        
-        int h = hours + timeToAdd.hours, 
-            m = minutes + timeToAdd.minutes, 
-            s = seconds + timeToAdd.seconds;
-        if (seconds + timeToAdd.seconds >= 60) {
-            s -= 60;
-            m += 1;
-        }
-        if (minutes + timeToAdd.minutes >= 60) {
-            m -= 60;
-            h += 1;
-        }
-        if (hours + timeToAdd.hours >= 24) {
-            h -= 24;
-        }
-        Time t(h, m, s);
+    Time& convertToTime() {
+        int h, m, s;
+        std::tie(h, m, s) = convertToTime_tuple(hours, minutes, seconds);
+        Time tt(h, m, s);
+        return tt;
+    }
+    Time addTimeInterval(const Time& timeToAdd) {
+        Time t;
+        t.hours = hours + timeToAdd.hours;
+        t.minutes = minutes + timeToAdd.minutes;
+        t.seconds = seconds + timeToAdd.seconds;
+        /*std::cout << 1 << ": " << hours << ": " << minutes << ": " << seconds << std::endl;
+        std::cout << 2 << ": " << timeToAdd.hours << ": " << timeToAdd.minutes << ": " << timeToAdd.seconds << std::endl;
+        std::cout << 3 << ": " << h << ": " << m << ": " << s << std::endl;
+        std::cout << 4 << ": " << t.hours << ": " << t.minutes << ": " << t.seconds << std::endl;*/
         return t;
     }
 
-    Time& subtractTimeInterval(const Time& timeToSubtract) {
-        int h = hours - timeToSubtract.hours,
-            m = minutes - timeToSubtract.minutes,
-            s = seconds - timeToSubtract.seconds;
-        if (seconds - timeToSubtract.seconds < 0) {
-            s = 60 + (seconds - timeToSubtract.seconds);
-            m -= 1;
-        }
-        if (minutes - timeToSubtract.minutes < 0) {
-            m = 60 + (minutes + timeToSubtract.minutes);
-            h -= 1;
-        }
-        if (hours - timeToSubtract.hours < 0) {
-            h = 24 + (hours - timeToSubtract.hours);
-        }
-        Time t(h, m, s);
+    Time subtractTimeInterval(const Time& timeToSubtract) {
+        Time t;
+        t.hours = hours - timeToSubtract.hours;
+        t.minutes = minutes - timeToSubtract.minutes;
+        t.seconds = seconds - timeToSubtract.seconds;
         return t;
+    }
+
+private:
+    std::tuple<int, int, int> convertToTime_tuple(int h, int m, int s) {
+        int hh = h, mm = m, ss = s;
+        const int min_sec = (h > 0 && m > 0 && s > 0) ? 60 : -60;
+        const int hrs = (h > 0 && m > 0 && s > 0) ? 24 : -24;
+        const int one = (h > 0 && m > 0 && s > 0) ? 1 : -1;
+        if (s >= 60 || s < 0) {
+            while (ss < 0 || ss >= 60) {
+                ss -= min_sec;
+                mm += one;
+            }
+        }
+        if (m >= 60 || m < 0) {
+            while (mm < 0 || mm >= 60) {
+                mm -= min_sec;
+                hh += one;
+            }
+        }
+        if (h >= 24 || h < 0) {
+            while (hh >= 24 || hh < 0) {
+                hh -= hrs;
+            }
+        }
+    
+        return std::make_tuple(hh, mm, ss);
     }
 };
 
@@ -71,29 +77,39 @@ Time& convert(std::string& input) {
     return t;
 }
 
-void ex1() {
-    char input[9];
-    std::cout << "Input time: (hh:mm:ss)\n";
-    std::cin.getline(input, 9);
-    std::string s = &input[0];
+//void ex1() {
+//    char input[9];
+//    std::cout << "Input time: (hh:mm:ss)\n";
+//    std::cin.getline(input, 9);
+//    std::string s = &input[0];
+//
+//    Time tt = convert(s);
+//
+//    std::cout << tt.hours << ":" << tt.minutes << ":" << tt.seconds << std::endl;
+//}
+//
+//void test() {
+//    Time t{ 15, 55, 32 };
+//    Time t2{ 21, 44, 50 };
+//    Time t3 = t.addTimeInterval(t2);
+//    std::cout << t3.hours << ":" << t3.minutes << ":" << t3.seconds << std::endl;
+//    Time tt(15, 55 + 44, 64);
+//    std::cout << tt.hours << ":" << tt.minutes << ":" << tt.seconds << std::endl;
+//}
 
-    Time tt = convert(s);
-
-    std::cout << tt.hours << ":" << tt.minutes << ":" << tt.seconds << std::endl;
-}
-
-void test() {
-    Time t{ 15, 55, 32 };
-    Time t2{ 21, 44, 50 };
-    Time t3 = t.addTimeInterval(t2);
-    std::cout << t3.hours << ":" << t3.minutes << ":" << t3.seconds << std::endl;
-    Time tt(15, 55 + 44, 64);
+void test2() {
+    Time t{ -15, -55, -32 };
+    std::cout << t.hours << ":" << t.minutes << ":" << t.seconds << std::endl;
+    Time t1{ 12, 34, 10 };
+    Time t2{ 10, 10, 10 };
+    Time tt = t.addTimeInterval(t1).addTimeInterval(t2).convertToTime();
     std::cout << tt.hours << ":" << tt.minutes << ":" << tt.seconds << std::endl;
 }
 
 int main()
 {
-   ex1();
-   test();
+   //ex1();
+   //test();
+    test2();
 }
 
