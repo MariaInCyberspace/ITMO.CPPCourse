97d0cafa5d602c66585dd209c63f83ce1f2a4d21
Maria Spirid <lan194@hotmail.com>
2022-01-27 17:35:58 +0300

    Non-mandatory ex. 1 of Lab 9

---
+++ Labs/Lab09/DivideByZero/DivideByZero/DivideByZero.cpp
@@ -0,0 +1,44 @@
+﻿// DivideByZero.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+class DivideByZeroError
+{
+public:
+    DivideByZeroError() : message("Divide by zero") { }
+    void printMessage() const { cout << message << endl; }
+private:
+    string message;
+};
+
+float quotient(int numl, int num2)
+{
+    if (num2 == 0)
+        throw DivideByZeroError();
+    return (float)numl / num2;
+}
+
+
+
+int main()
+{
+    cout << "Enter two integers to calculate their quotient:\n";
+    int number1, number2;
+    cin >> number1;
+    cin >> number2;
+    try
+    {
+        float result = quotient(number1, number2);
+        cout << "Quotient is " << result << endl;
+    }
+    catch (DivideByZeroError& error)
+    {
+        cout << "Error : ";
+        error.printMessage();
+        return 1; 
+    }
+    return 0;
+}
