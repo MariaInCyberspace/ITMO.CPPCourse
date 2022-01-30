2362048afb6c8745d049c315134ea263cc58b7d6
Maria Spirid <lan194@hotmail.com>
2022-01-30 12:05:55 +0300

    Non-mandatory ex. 2 of Lab 14

--- Labs/Lab14/ArraysAndTemplateFunctions/ArraysAndTemplateFunctions/ArraysAndTemplateFunctions.cpp
+++ Labs/Lab14/ArraysAndTemplateFunctions/ArraysAndTemplateFunctions/ArraysAndTemplateFunctions.cpp
@@ -2,7 +2,14 @@
 //
 
 #include <iostream>
+#include <windows.h>
+#include <string>
+#include <tuple>
+#include <vector>
 using namespace std;
+
+template<class Tuple>
+void printTupleOfThree(Tuple t);
 
 template<class T>
 void sorting(T arr[], int size) {
@@ -21,8 +28,7 @@ void display(T arr[], int size) {
     cout << endl;
 }
 
-int main()
-{
+void ex1() {
     int arr[] = { 9,3,17,6,5,4,31,2,12 };
     double arrd[] = { 2.1, 2.3,1.7,6.6,5.3,2.44,3.1,2.4,1.2 };
     char arrc[] = "Hello, word";
@@ -32,9 +38,35 @@ int main()
     sorting(arr, k1);
     sorting(arrd, k2);
     sorting(arrc, k3);
-   
+
     display(arr, k1);
     display(arrd, k2);
     display(arrc, k3);
+}
+
+void ex2() {
+    vector<string> v1{ "one", "two", "three", "four", "five", "six" };
+    vector<int> v2 = { 1, 2, 3, 4, 5, 6 };
+    vector<double> v3 = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+
+    auto t0 = make_tuple(v1[0], v2[0], v3[0]);
+
+    printTupleOfThree(t0);
+
+}
+
+template<class Tuple>
+void printTupleOfThree(Tuple t)
+{
+    cout << "("
+        << std::get<0>(t) << ", "
+        << std::get<1>(t) << ", "
+        << std::get<2>(t) << ")" << endl;   
+}
+
+
+int main()
+{
+    ex2();
 }
 
