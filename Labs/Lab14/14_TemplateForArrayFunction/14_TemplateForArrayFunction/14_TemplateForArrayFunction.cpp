3651f672edf3b4704a32d68e1ebdcc4cd818232c
Maria Spirid <lan194@hotmail.com>
2022-01-30 14:12:44 +0300

    Lab 14

---
+++ Labs/Lab14/14_TemplateForArrayFunction/14_TemplateForArrayFunction/14_TemplateForArrayFunction.cpp
@@ -0,0 +1,27 @@
+﻿// 14_TemplateForArrayFunction.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+template<class T>
+T averageValue(T arr[], int size) {
+    T sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return (sum / size);
+}
+
+int main()
+{
+    int intArr[]{ 1, 1, 1, 1, 1 };
+    double doubleArr[]{ 1.1, 1.1, 1.2, 1.1, 1.1 };
+    long longArr[]{ 1, 2, 1, 3, 1 };
+    char charArr[] = "GeeksforGeeks";
+
+    std::cout << averageValue(intArr, 5) << "\n";
+    std::cout << averageValue(doubleArr, 5) << "\n";
+    std::cout << averageValue(longArr, 5) << "\n";
+    std::cout << averageValue(charArr, sizeof(charArr) / sizeof(charArr[0])) << "\n";
+}
---
+++ Labs/Lab14/ArrayTemplateClass/ArrayTemplateClass/ArrayTemplateClass.cpp
@@ -0,0 +1,83 @@
+﻿// ArrayTemplateClass.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+template<class T, class T1> class arr {
+public:
+    arr(int size);
+    ~arr() { delete data; cout << "destructor\n"; }
+    T1 sum();
+    T averageValue();
+    void showArray();
+    int addValue(T value);
+private:
+    T* data;
+    int size;
+    int index;
+};
+
+template<class T, class T1>
+arr<T, T1>::arr(int size)
+{
+    data = new T[size];
+    if (data == NULL)
+    {
+        cerr << "Error memory ---- exit program" << endl;
+        exit(1);
+    }
+    arr::size = size;
+    arr::index = 0;
+}
+
+template<class T, class T1>
+T1 arr<T, T1>::sum()
+{
+    T1 sum = 0;
+    for (int i = 0; i < index; i++) sum += data[i];
+    return(sum);
+}
+
+template<class T, class T1>
+T arr<T, T1>::averageValue()
+{
+    T1 sum = 0;
+    for (int i = 0; i < index; i++) sum += data[i];
+    return (sum / index);
+}
+
+template<class T, class T1>
+void arr<T, T1>::showArray()
+{
+    for (int i = 0; i < index; i++) cout << data[i] << ' ';
+    cout << endl;
+}
+
+template<class T, class T1>
+int arr<T, T1>::addValue(T value)
+{
+    if (index == size)
+        return(-1);
+    else
+    {
+        data[index] = value;
+        index++;
+        return(0);
+    }
+}
+
+int main()
+{
+    arr<int, long> numbers(100);
+    arr<float, float> values(200);
+    int i;
+    for (i = 0; i < 50; i++) numbers.addValue(i);
+    numbers.showArray();
+    std::cout << "Sum = " << numbers.sum() << std::endl;
+    std::cout << "Average = " << numbers.averageValue() << std::endl;
+    for (i = 0; i < 100; i++) values.addValue(i * 100);
+    values.showArray();
+    std::cout << "Sum = " << values.sum() << std::endl;
+    std::cout << "Average = " << values.averageValue() << std::endl;
+}
